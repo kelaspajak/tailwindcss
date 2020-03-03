238561f7ab963c289199dc5d52518d81f9cdb3ea
berlamhenderson <less.summerize@gmail.com>
2020-03-03 16:41:03 +0800

    not initial setup

--- _includes/tailwind.config.js
+++ _includes/tailwind.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   theme: {
   },
-  variants: {},
+  variants: {
+    borderWidth: ['responsive', 'first', 'hover', 'focus'],
+  },
   plugins: [],
 }
--- purgecss.config.js
+++ purgecss.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   content: ["./_site/**/*.html"],
   css: ["./_site/css/site.css"],
+  css: ["./assets/css/site.css"],
   defaultExtractor: content => content.match(/[A-Za-z0-9-_:/]+/g) || []
 };
